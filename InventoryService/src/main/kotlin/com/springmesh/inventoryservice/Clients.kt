62d31316841374fcfbf6174266a73ebb41c7a9af
Zeid Adabel <Zeid.Adabel@ortec-finance.com>
2022-06-15 11:25:58 +0200

    Adding connections to clients

--- InventoryService/src/main/kotlin/com/springmesh/inventoryservice/Clients.kt
+++ InventoryService/src/main/kotlin/com/springmesh/inventoryservice/Clients.kt
@@ -1,4 +1,4 @@
-package com.springmesh.userservice
+package com.springmesh.inventoryservice
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -8,13 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient
 class Clients(
     @Value("\${debug}")
     private val debug: String
-) {
+)  {
     @Bean
-    fun InventoryService(): WebClient? {
+    fun ItemService(): WebClient? {
         return if(this.debug.toBoolean()){
-            WebClient.create("http://localhost:8084/api/")
+            WebClient.create("http://localhost:8082/api/")
         } else{
-            WebClient.create("http://InventoryService:8080/api/")
+            WebClient.create("http://ItemService:8080/api/")
         }
     }
 }
--- PurchaseService/src/main/kotlin/com/springmesh/purchaseservice/Clients.kt
+++ PurchaseService/src/main/kotlin/com/springmesh/purchaseservice/Clients.kt
@@ -1,4 +1,4 @@
-package com.springmesh.userservice
+package com.springmesh.purchaseservice
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -8,13 +8,22 @@ import org.springframework.web.reactive.function.client.WebClient
 class Clients(
     @Value("\${debug}")
     private val debug: String
-) {
+)  {
     @Bean
     fun InventoryService(): WebClient? {
         return if(this.debug.toBoolean()){
             WebClient.create("http://localhost:8084/api/")
         } else{
             WebClient.create("http://InventoryService:8080/api/")
+        }
+    }
+
+    @Bean
+    fun ItemService(): WebClient? {
+        return if(this.debug.toBoolean()){
+            WebClient.create("http://localhost:8082/api/")
+        } else{
+            WebClient.create("http://ItemService:8080/api/")
         }
     }
 }
