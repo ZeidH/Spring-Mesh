4c998af007cde680c47aaf0079f007d175f3c322
Zeid Adabel <Zeid.Adabel@ortec-finance.com>
2022-06-22 16:00:05 +0200

    Created build scripts

---
+++ InventoryService/BuildS2i.bat
@@ -0,0 +1,2 @@
+IF EXIST "target" RMDIR /S /Q "target"
+s2i build . registry.access.redhat.com/ubi8/openjdk-11 ofocp.azurecr.io/navigator/mesh/InventoryService:main -e GC_MAX_METASPACE_SIZE=2000 -e AB_JOLOKIA_OFF=true -c --incremental
