6516dcfcb25ebf5b83cd36d539556e7754bd8d60
Zeid Adabel <Zeid.Adabel@ortec-finance.com>
2022-06-22 16:49:17 +0200

    Added environment variable to builds

--- PurchaseService/BuildS2i.bat
+++ PurchaseService/BuildS2i.bat
@@ -1,2 +1,2 @@
 IF EXIST "target" RMDIR /S /Q "target"
-s2i build . registry.access.redhat.com/ubi8/openjdk-11 ofocp.azurecr.io/navigator/mesh/PurchaseService:main -e GC_MAX_METASPACE_SIZE=2000 -e AB_JOLOKIA_OFF=true -c --incremental
+s2i build . registry.access.redhat.com/ubi8/openjdk-11 ofocp.azurecr.io/navigator/mesh/purchase-service:main -e GC_MAX_METASPACE_SIZE=2000 -e AB_JOLOKIA_OFF=true -e JAVA_OPTS=-Dspring.profiles.active=cloud -c --incremental
